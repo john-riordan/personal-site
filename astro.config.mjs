708b39335155bc22a3693f479c44ed84e7f5911e
John Riordan <iam.john.riordan@gmail.com>
2022-09-25 13:59:19 -0700

    feat: add layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
-	// Enable React to support React JSX components.
-	integrations: [react()]
+  // Enable React to support React JSX components.
+  integrations: [react(), svelte()]
 });
---
+++ src/components/ThemeToggle.svelte
@@ -0,0 +1,24 @@
+<script>
+  let theme = "dark"
+
+  function toggleTheme() {
+    const body = document.querySelector("body");
+
+    if (theme === "dark") {
+      theme = "light";
+      body.classList.remove("dark");
+      body.classList.add("light");
+    } else {
+      theme = "dark";
+      body.classList.remove("light");
+      body.classList.add("dark");
+    }
+
+    
+    // if (body) body.toggle
+  }
+</script>
+
+<button on:click={toggleTheme}>
+  Toggle Theme
+</button>
