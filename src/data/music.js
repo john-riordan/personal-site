ff9268bb0ec8e6d4e4b755f95f56fff2b7059317
John Riordan <iam.john.riordan@gmail.com>
2023-01-08 18:22:09 -0800

    Music 2023 project (#3)
    * feat: weekly music project
    
    * feat: content update
    
    * feat: music and header updates

---
+++ src/data/music.js
@@ -0,0 +1,65 @@
+export default [
+	{
+		slug: 'week-1',
+		week: 1,
+		month: 'Jan',
+		date: new Date('January 6, 2023 00:00:00'),
+		dw: '3nboe40m3nAxKVm5nCXSql',
+		rr: '351nTThZ1Lcq2pdP5RVPwm',
+		aboutFavs: `
+			Most of the music I listen to these days is whatever I get from my
+			<a href="#dw">Discover Weekly</a> and <a href="#rr">Release Radar</a>. Here are some of the
+			ones that stood out this week.
+			<br /><br />
+			I'd say this is a good start to the year.
+		`,
+		favs: [
+			{
+				id: '4wM1e6S2jJrvxdK5qIYj5q',
+				name: 'Jetpacks',
+				artist: 'Von Kaiser',
+				notes: `
+					Big shocker a synthwave song 😅. The little guitar riff during the hook 
+					sounds an awful lot like riff from <a href="https://open.spotify.com/track/4PMmwowVLOajPdiKnrU1vK?si=9295301e20594854">Sunset</a> 
+					by The Midnight (one of my favorite bands). 
+					This is what I call driving music. I can totally jam out and belt to this.
+				`
+			},
+			{
+				id: '6ZbE8insnDOqnawvzZ3I7R',
+				name: 'Sky Fleur',
+				artist: 'Late June',
+				notes: `
+					I've never heard of Late June before this calming, rhythmic, instrumental tone is exactly the
+					type of music I like to work to.
+				`
+			},
+			{
+				id: '53gVj6cwOhDqbjufY30J7c',
+				name: 'Liberty of Broken Bindings',
+				artist: 'Sincere Engineer',
+				notes: `
+					Sincere Engineer's sound reminds me a bit of one of my favorites 
+					<a href="https://open.spotify.com/artist/1r1uxoy19fzMxunt3ONAkG?si=5e24169818d94b5d">Phoebe Bridgers 🎶.</a> 
+					So naturally this was an instant hit for me.`
+			},
+			{
+				id: '5UqTWnG7YZAbj1SYMvnGsU',
+				name: 'Scarlett',
+				artist: 'Holly Humberstone',
+				notes: ''
+			},
+			{
+				id: '10yYDp1Lnac8zEIYiCk1sp',
+				name: 'Wish on an Eyelash, Pt. 2',
+				artist: 'Mallrat, The Chainsmokers',
+				notes: `
+					I'm not particularly a Chainsmokers fan, but I am 
+					indeed a Mallrat fan and I just recently stumbled upon 
+					<a href="https://open.spotify.com/track/3vLByi1CdmNJPpTtOdKqAQ?si=1207724d9e9e4c38"> the song this is based on</a> 
+					(the first 40s of this) and  this is a fun little extension of it.
+				`
+			}
+		]
+	}
+];
