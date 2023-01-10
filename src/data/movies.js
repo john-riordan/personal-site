5ad78034deb5a28357bbc1fc83fb59a066f1c7f2
John Riordan <iam.john.riordan@gmail.com>
2023-01-09 22:44:52 -0800

    feat: start movie list

---
+++ src/data/movies.js
@@ -0,0 +1,15 @@
+export default [
+	{
+		slug: 'the-banshees-of-inisherin',
+		title: 'The Banshees of Inisherin',
+		// description: `
+		//   Here is some words about what I thought about The Banshees of Inisherin.
+		//   It's a few sentences long at least.
+		// `,
+		date: new Date('January 8, 2023 10:00:00'),
+		imdb: 'https://www.imdb.com/title/tt11813216/',
+		rating: 4,
+		poster: 'banshees.webp',
+		youtube: 'uRu3zLOJN2c'
+	}
+];
