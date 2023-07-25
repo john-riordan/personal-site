17239e2c4cdbcc6ddd9e2f24f9960d4cde71dc88
John Riordan <iam.john.riordan@gmail.com>
2023-07-24 20:55:46 -0700

    feat: bump astro and tiny color adjustment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-
-import svelte from "@astrojs/svelte";
+import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-  // Enable React to support React JSX components.
-  integrations: [react(), svelte()]
+	integrations: [svelte()]
 });
