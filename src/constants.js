cf2920371a2383f53240c25e8c945bd4c576451d
John Riordan <iam.john.riordan@gmail.com>
2023-07-24 21:39:25 -0700

    feat: update discord handle

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,10 @@
 const EMAIL = 'iam.john.riordan@gmail.com';
+
 export const SOCIALS = {
 	twitter: 'https://twitter.com/johnriordan',
 	linkedin: 'https://www.linkedin.com/in/riordanjohn/',
 	email: EMAIL,
 	emailWithSubject: `mailto:${EMAIL}?subject=Yo Rio, HUGE fan 🙌`,
-	github: 'https://github.com/john-riordan'
+	github: 'https://github.com/john-riordan',
+	discord: 'john_riordan'
 };
