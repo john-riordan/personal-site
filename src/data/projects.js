867543305a3599cf595a619a4b3d7e6cdc27a6fa
John Riordan <iam.john.riordan@gmail.com>
2024-09-22 14:05:25 -0700

    chore: update astro and cleanup css vars

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,34 +1,34 @@
 export default [
-  {
-    url: "https://championsqueue.gg",
-    title: "ChampionsQueue.gg",
-    description: `My latest joint for the League of Legends community 🎮. Focused around providing the best possible
+	{
+		url: "https://championsqueue.gg",
+		title: "ChampionsQueue.gg",
+		description: `My latest joint for the League of Legends community 🎮. Focused around providing the best possible
 		UX experience for the NA Champions Queue competitive ladder. The <a href="https://championsqueue.lolesports.com/en-us/">official site</a>
 		 just wasn't cutting it so I wanted to see how far I could push the limits of UX using the exact same data the offial site uses.`,
-    tags: ["Svelte", "SPA", "League of Legends"]
-  },
-  {
-    url: "https://lolshop.gg",
-    title: "LoL Shop",
-    description: `Like my other project RuneForge, Lol Shop was designed as an educational tool to introduce 
+		tags: ["Svelte", "SPA", "League of Legends"]
+	},
+	{
+		url: "https://lolshop.gg",
+		title: "LoL Shop",
+		description: `Like my other project RuneForge, Lol Shop was designed as an educational tool to introduce 
 		and guide players into the comprehensieve 
 		<a href="https://www.leagueoflegends.com/en-pl/news/dev/preseason-2021-mythic-legendary-items-preview/">LoL item overhaul</a>. 
 		Starting to see a bit of a pattern? 😉`,
-    tags: ["React", "SPA", "League of Legends"]
-  },
-  {
-    url: "https://runeforge.gg",
-    title: "RuneForge (currently offline)",
-    description: `An educational web-app with the goal of helping to ease players into the 
+		tags: ["React", "SPA", "League of Legends"]
+	},
+	{
+		url: "https://runeforge.gg",
+		title: "RuneForge (currently offline)",
+		description: `An educational web-app with the goal of helping to ease players into the 
 		massive League of Legends <a href="https://na.leagueoflegends.com/en/featured/preseason-update">Season 8 pre-season" 
 		Runes Reforged"</a> update.`
-  },
-  {
-    url: "https://worldforge.gg",
-    title: "World Forge",
-    description: `Turns out I'm a massive MMO nerd 🔮. My MMO roots go way back. When <a href="https://www.newworld.com/en-us/">New World</a> was announced,
+	},
+	{
+		url: "https://worldforge.gg",
+		title: "World Forge",
+		description: `Turns out I'm a massive MMO nerd 🔮. My MMO roots go way back. When <a href="https://www.newworld.com/en-us/">New World</a> was announced,
 		I couldn't help myself but to dip my toe into exploring opportunities to apply my UX problem solving skills
 		to the unique game design problems of the game.`,
-    tags: ["Svelte", "SPA", "New World"]
-  }
+		tags: ["Svelte", "SPA", "New World"]
+	}
 ];
